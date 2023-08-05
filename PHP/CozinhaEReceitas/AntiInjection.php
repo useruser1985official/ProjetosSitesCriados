d9bdf6b55713b1b43d917d250958e34ef946b364
User User <useruser1985official@outlook.com>
2023-08-04 21:04:04 -0300

    Atualização em Massa

--- PHP/CozinhaEReceitas/AntiInjection.php
+++ PHP/CozinhaEReceitas/AntiInjection.php
@@ -5,11 +5,15 @@ class AntiInjection {
     private static $listaNegra = array("select", "update", "drop", "truncate", "insert", "delete", "alter", "from", "where", "table", "tables", "database", "union", "--", "%", "<", ">", "[", "]", ":", "?", "`", "|", "*");
 
     public function texto($frase) : string {
-        $this->limpa = str_ireplace(";", "", $frase);
-        $this->limpa = str_ireplace("--", "", $this->limpa);
-        $this->limpa = str_ireplace("/*", "", $this->limpa);
-        $this->limpa = str_ireplace("*/", "", $this->limpa);
+        $this->limpa = str_ireplace(";", "&#59;", $frase);
+        $this->limpa = str_ireplace("--", "&#45;&#45;", $this->limpa);
+        $this->limpa = str_ireplace("*", "&#42;", $this->limpa);
+        $this->limpa = str_ireplace("=", "&#61;", $this->limpa);
         $this->limpa = htmlentities($this->limpa, ENT_QUOTES);
+        $this->limpa = str_ireplace("amp;", "", $this->limpa);
+        $this->limpa = str_ireplace("&&", "&amp;&amp;", $this->limpa);
+        $this->limpa = str_ireplace("||", "&#124;&#124;", $this->limpa);
+        $this->limpa = str_ireplace("!", "&#33;", $this->limpa);
         $this->limpa = strip_tags($this->limpa);
         $this->limpa = trim($this->limpa);
 
