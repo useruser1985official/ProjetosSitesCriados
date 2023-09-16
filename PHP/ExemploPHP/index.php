c5a12c370043adecd67956bcbe8edeba5aa41e92
User User <useruser1985official@outlook.com>
2023-09-16 11:37:12 -0300

    Atualização Semanal

---
+++ PHP/ExemploPHP/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Exemplo PHP</title>
+    </head>
+    <body>
+        <?php
+            $x = 0;
+
+            while($x < 10) {
+                echo "O número é $x.<br/>";
+                $x++;
+            }
+        ?>
+    </body>
+</html>
