bac59fe3439e1dac1c6ffd6c41cfcad853fc6037
User User <useruser1985official@outlook.com>
2025-06-13 19:17:19 -0300

    Atualização Semanal

--- HTML e JS/PraticaJQuery/javascript/funcoes.js
+++ HTML e JS/PraticaJQuery/javascript/funcoes.js
@@ -1,11 +1,19 @@
 $(function() {
-    var ex = $(".ex1");
-    var ex2 = $(".ex2");
-    var button = $(":input.ev1");
+    $("#imagem1").height(300);
+    $("#imagem2").width(200);
 
-    button.click(function() {
-        ex.fadeTo(3000, 0.6, function() {
-            ex2.fadeTo("slow", 0.3);
-        });
+    $("#form1").on("input", function() {
+        var img1 = $("#num1").val();
+        var img2 = $("#num2").val();
+
+        $("#imagem1").height(img1);
+        $("#imagem2").width(img2);
+    });
+
+    $("#resetar").on("click", function() {
+        $("#imagem1").height(300);
+        $("#imagem2").width(200);
+        $("#num1").val(300);
+        $("#num2").val(200);
     });
 });
