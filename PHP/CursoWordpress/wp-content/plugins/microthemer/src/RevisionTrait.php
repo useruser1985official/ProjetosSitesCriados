7191d068e459af3c51054675a3b3efc9e722e831
User User <useruser1985official@outlook.com>
2025-09-19 19:27:16 -0300

    Atualização Semanal

--- PHP/CursoWordpress/wp-content/plugins/microthemer/src/RevisionTrait.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Microthemer;
-
-trait RevisionTrait {
-
-
-}
--- PHP/CursoWordpress/wp-content/themes/astra/admin/assets/build/dashboard-app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'ae53d0bbac37b1791551');
